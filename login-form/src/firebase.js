6c60bd4b3e192dfe25b5702a5b97d11508a34ceb
DEWANGSHARMA06 <165178163+DEWANGSHARMA06@users.noreply.github.com>
2025-04-02 23:47:20 +0530

    asd

--- login-form/src/firebase.js
+++ login-form/src/firebase.js
@@ -2,21 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// firbase connection done
+import { getAnalytics } from "firebase/analytics";
 // 🔥 Your Firebase Config (Replace with actual credentials)
 const firebaseConfig = {
-    apiKey: "AIzaSyBhNOmK2EoPfo_qTXDVIFQ5WDoOjsw6X20",
-    authDomain: "users-registered.firebaseapp.com",
-    projectId: "users-registered",
-    storageBucket: "users-registered.firebasestorage.app",
-    messagingSenderId: "849481680658",
-    appId: "1:849481680658:web:9c6d6a65b2a320946c15cb"
+    apiKey: "AIzaSyCsJniBaoMRhfLAxtwZcL-IbMe_PtMF9Xk",
+  authDomain: "agrilink-99627.firebaseapp.com",
+  projectId: "agrilink-99627",
+  storageBucket: "agrilink-99627.firebasestorage.app",
+  messagingSenderId: "96428598095",
+  appId: "1:96428598095:web:24b6180e23d363d60e1b3a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // Export Firebase instances
-export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword };
